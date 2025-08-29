89f4d3bf4b31935c14c12bfa39bedf3f18c35f10
Ajaynirm <ajaysam397@gmail.com>
2025-08-29 15:40:40 +0530

    db config updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,12 @@
-import { createPool } from "mysql2";
+import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
-const pool = createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false,     
-  },
+const pool = mysql.createPool({
+  uri: process.env.DB_URI,
+    ssl: {
+      rejectUnauthorized: false, 
+    },
 });
 
 pool.getConnection((err, connection) => {
